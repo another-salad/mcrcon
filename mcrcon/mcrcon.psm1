bb9a5d4b5ee890d267227356b075ea54ab742f7f
anothersalad <48966874+another-salad@users.noreply.github.com>
2025-01-09 01:40:23 +0000

    part 1, too tired to test

--- mcrcon/mcrcon.psm1
+++ mcrcon/mcrcon.psm1
@@ -8,43 +8,109 @@ enum RConPacketType {
     SERVERDATA_RESPONSE_VALUE = 0
 }
 
-Function New-RConPayloadPacket {
-    [CmdletBinding()]
-    param (
-        # I would love to use the enum here, but it is far to awkward in a ValidateSet
-        [Parameter(Mandatory)]
-        [ValidateSet(3, 2)]
-        [int]$Type,
-        [Parameter(Mandatory)]
-        [string]$Command
-    )
-    # 4 bytes for the packet id, and type
-    $PktId = [byte[]]::new(4)
-    [System.Security.Cryptography.RandomNumberGenerator]::Create().GetBytes($PktId)
-    $PktCmdType = [byte[]]::new(4)
-    $PktCmdType[0] = $Type
-    # The command string, null terminated
-    $PktCmdPayload = [System.Text.Encoding]::ASCII.GetBytes($Command) + 0x00
-    $PktSize = [BitConverter]::GetBytes($PktCmdPayload.Length + 9)
-    # The full packet, in the required structure
-    $PktSize + $PktId + $PktCmdType + $PktCmdPayload + 0x00
+class RconCommand {
+    [string]$Command
+    [ValidateSet(2, 3)]
+    [int]$Type = ([int][RConPacketType]::SERVERDATA_EXECCOMMAND)
+
+    RconCommand([string]$Command, [int]$Type) {
+        $this.Command = $Command
+        $this.Type = $Type
+    }
+
+    RconCommand([string]$Command) {
+        $this.Command = $Command
+    }
 }
 
-Function New-RawRconSocket {
+Function New-RconCommand {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$Command,
+        [Parameter()]
+        [int]$Type = ([int][RConPacketType]::SERVERDATA_EXECCOMMAND)
+    )
+    [RconCommand]::new($Command, $Type)
+}
+
+class RconSession {
+    [System.Net.Sockets.Socket]$Socket
+    [string]$Address
+    [int]$Port
+    [securestring]$Password
+
+    RconConnection([string]$Address, [int]$Port, [securestring]$Password) {
+        $this.Address = $Address
+        $this.Port = $Port
+        $this.Password = $Password
+        $this.Connect()
+        $this.Authenticate()
+    }
+
+    [void] Connect() {
+        $_Socket = [System.Net.Sockets.Socket]::New(
+            [System.Net.Sockets.AddressFamily]::InterNetwork,
+            [System.Net.Sockets.SocketType]::Stream,
+            [System.Net.Sockets.ProtocolType]::Tcp
+        )
+        $_Socket.Connect($this.Address, $this.Port)
+        $this.Socket = $_Socket
+    }
+
+    [void] Authenticate() {
+        $ResponseBuffer = $this.Send((New-RconCommand -Command ($this.Password | ConvertFrom-SecureString -AsPlainText) -Type ([int][RConPacketType]::SERVERDATA_AUTH)))
+        if ([BitConverter]::ToInt32($ResponseBuffer[4..7], 0) -eq -1) {
+            Write-Error "Authentication failed, bad password?"
+        }
+    }
+
+    [byte[]] Packet([RconCommand]$RconCommand) {
+        $PktId = [byte[]]::new(4)
+        [System.Security.Cryptography.RandomNumberGenerator]::Create().GetBytes($PktId)
+        $PktCmdType = [byte[]]::new(4)
+        $PktCmdType[0] = $RconCommand.Type
+        # The command string, null terminated
+        $PktCmdPayload = [System.Text.Encoding]::ASCII.GetBytes($RconCommand.Command) + 0x00
+        $PktSize = [BitConverter]::GetBytes($PktCmdPayload.Length + 9)
+        # The full packet, in the required structure
+        return $PktSize + $PktId + $PktCmdType + $PktCmdPayload + 0x00
+    }
+
+    [byte[]] Send([RconCommand]$RconCommand) {
+        if (-not $this.Socket.Connected) {
+            $this.Connect()
+            if (-not $RconCommand.Type -eq [int][RConPacketType]::SERVERDATA_AUTH) {
+                Write-Warning "Socket was dead, attempting to Re-Authenticate"
+                $this.Authenticate()
+            }
+        }
+        $this.Socket.Send($this.Packet($RconCommand)) | Out-Null
+        $ResponseBuffer = [byte[]]::new(4096)
+        $this.Socket.Receive($ResponseBuffer) | Out-Null
+        return $ResponseBuffer
+    }
+
+    [void] Close() {
+        if ($null -ne $this.Socket -and $this.Socket.Connected) {
+            $this.Socket.Close()
+        }
+        $this.Socket = $null
+    }
+
+}
+
+Function New-RconSession {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
         [string]$Address,
         [Parameter(Mandatory)]
-        [int]$Port
+        [int]$Port,
+        [Parameter(Mandatory)]
+        [securestring]$Password
     )
-    $Socket = [System.Net.Sockets.Socket]::New(
-        [System.Net.Sockets.AddressFamily]::InterNetwork,
-        [System.Net.Sockets.SocketType]::Stream,
-        [System.Net.Sockets.ProtocolType]::Tcp
-    )
-    $Socket.Connect($Address, $Port)
-    $Socket
+    [RconSession]::new($Address, $Port, $Password)
 }
 
 Function Set-InteractiveSessionPassword {
@@ -62,30 +128,26 @@ Function Get-RconPasswordFromServerProperties {
     (Get-Content $ServerPropertiesPath) | Where-Object { $_ -match $rgx  } | ForEach-Object { $_ -replace $rgx , '' } | ConvertTo-SecureString -AsPlainText -Force
 }
 
-Function New-ResponseBuffer {
+Function Send-RconCommand {
     [CmdletBinding()]
     param (
-        [int]$Size = 4096
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [RconSession]$Session,
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [RconCommand]$Command
     )
-    [byte[]]::new($Size)
+    process {
+        $Session.Send($Command)
+    }
 }
 
-Function New-RconSession {
+Function Get-Status {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory)]
-        [string]$Address,
-        [Parameter(Mandatory)]
-        [int]$Port,
-        [Parameter(Mandatory)]
-        [securestring]$Password
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [RconSession]$Session
     )
-    $Socket = New-RawRconSocket -Address $Address -Port $Port
-    $Socket.Send((New-RConPayloadPacket -Type ([int][RConPacketType]::SERVERDATA_AUTH) -Command ($Password | ConvertFrom-SecureString -AsPlainText))) | Out-Null
-    $ResponseBuffer = New-ResponseBuffer
-    $Socket.Receive($ResponseBuffer) | Out-Null
-    if ([BitConverter]::ToInt32($ResponseBuffer[4..7], 0) -eq -1) {
-        Write-Error "Authentication failed, bad password?"
+    process {
+        $Session.Send((New-RconCommand -Command "status"))
     }
-    $Socket
 }
